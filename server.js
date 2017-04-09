ad8ce1de39514a8f73df7898fa77eceeb6155313
Ole Anders <git@swoy.org>
2017-04-09 14:50:52 +0200

    added quotes and authors in index html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function(request, response) {
-    res.sendFile(__dirname + 'index.html');
+    response.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.set('port', process.env.PORT || 8080);
+//app.set('port', 3000); //localhost testing
 app.listen(app.get('port'));
